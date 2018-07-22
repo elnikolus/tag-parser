b9c952e272bce776950c8fd5c87cff9d0bfb9125
elnikolus <elnikolus@gmail.com>
2018-07-22 19:19:36 +0300

    Parser class

---
+++ Parser.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'ParserInterface.php';
+
+/**
+ * @author NonElnikolus <elnikolus@gmail.com>
+ */
+class Parser implements ParserInterface
+{
+	
+	return [
+		'just',
+		'do',
+		'it'
+	];
+}
